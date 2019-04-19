40e93bc59b02dfb52a4a7e850afee0d590c23591
Nantipat Suwanwong <scemisnis@gmail.com>
2019-04-20 06:28:55 +0700

    init

---
+++ cashier/index.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <h1>Cashier</h1>
+    <a href="/Proj_com_store/">Home</a>
+    
+</body>
+</html>
---
+++ home/index.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="/Proj_com_store/css/style.css">
+</head>
+<body>
+    <h1>ร้านขายอุปกรณ์คอม~</h1>
+    <a href="/Proj_com_store/Cashier">Cashier</a>
+    <a href="/Proj_com_store/Productlist">Product list</a>
+    <a href="/Proj_com_store/Stock">Stock</a>
+</body>    
+</html>
