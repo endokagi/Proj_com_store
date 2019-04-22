71016a51b6d61d26ead8fbcff60e8d935b69dfeb
EnDoZaKi <38246228+endokagi@users.noreply.github.com>
2019-04-22 23:54:50 +0700

    งง

--- cashier/index.php
+++ cashier/index.php
@@ -18,27 +18,87 @@
         <a href="../productlist/index.php">Search products</a>
     </div>
     
+    <!-- <div>
+        <?php
+            $connect = mysqli_connect("localhost","root","","computerstore");
+            $sqlBrand = 'SELECT PName,PDetail,Price,Unit FROM product inner join brand on product.BrandID = brand.BrandID
+            inner join stock on product.ProductID = stock.ProductID';
+            $resultBrand = mysqli_query($connect,$sqlBrand);
+            echo '<table border=1>';
+            echo '<th>Product name</th><th>Product detail</th>
+            <th>price</th><th>Unit</th><th>ADD</th>';
+            while($row = mysqli_fetch_assoc($resultBrand)){
+                echo '<tr>';
+                while(list($key,$value) = each($row)){
+                    echo '<td>'.'<option value="'.$value.'">'.$value.'</option>'.'</td>';
+                }
+                echo '<td>'.'<button value="'.$value.' name="add">ADD</button>'.'</td>'.'</tr>';
+                $connect = mysqli_connect("localhost","root","","computerstore");
+                $sqlAdd = 'SELECT PName,PDetail,Price,Unit FROM product inner join brand on product.BrandID = brand.BrandID
+                inner join stock on product.ProductID = stock.ProductID';
+                $resultAdd = mysqli_query($connect,$sqlAdd);
+            }
+            echo '</table>';           
+            mysqli_close($connect);
+            $connect = mysqli_connect("localhost","root","","computerstore");
+            $sqlStock_Unit = 'SELECT Unit from stock';
+            mysqli_close($connect);
+            // if($sqlStock_Unit==0){
+            //     echo '<div id=""></div>';
+            // }else{
+            //     $sqlOrder = array();
+            //     for($i=0;$i<5;$i++){
+            //         $sqlOrder[$i] = 'SELECT PName,PDetail,Price,Unit FROM product inner join brand on product.BrandID = brand.BrandID
+            //         inner join stock on product.ProductID = stock.ProductID
+            //         where ProductID ='.$value;
+            //     }
+            // }
+        ?>
+        <form action="index2.php" method="post">
+            <button>Add products</button>
+        </form>
+    </div> -->
+
+    
+
 
     <div>
         <form action="index2.php" method="post">
+            
             <h1>Add product</h1><p></p>
-            Select brand
-            <select name="select_Brand">
-                <?php
-                    $connect = mysqli_connect("localhost","root","","computerstore");
-                    $sqlBrand = 'SELECT BName FROM brand';
-                    $resultBrand = mysqli_query($connect,$sqlBrand);
-                    while($row = mysqli_fetch_assoc($resultBrand)){
-                        while(list($key,$value) = each($row)){
-                            echo '<option value="'.$value.'">'.$value.'</option>';
-                        }
-                    }           
-                    mysqli_close($connect);
-                ?>
-            </select>
-            Name
-            <input type="text" name="product_name">
-            <button id="btnsub">Submit</button>
+            <?php
+                for($i=0;$i<5;$i++){
+                    echo"Select brand";
+                    echo '<select name=select_Brand>';
+                        $connect = mysqli_connect("localhost","root","","computerstore");
+                        $sqlBrand = 'SELECT BName FROM brand';
+                        $resultBrand = mysqli_query($connect,$sqlBrand);
+                        while($row = mysqli_fetch_assoc($resultBrand)){
+                            while(list($key,$value) = each($row)){
+                                echo '<option value="'.$value.'">'.$value.'</option>';
+                            }
+                        }           
+                        mysqli_close($connect);
+                    echo'</select>';
+                    
+                    echo "Name";
+                    echo '<input type="text" name="product_name">';
+                    echo '<select name=select_Name>';
+                        $connect = mysqli_connect("localhost","root","","computerstore");
+                        $sqlPName = 'SELECT PName FROM product inner join brand
+                        on product.BrandID = brand.BrandID
+                        where BName ="'.$value.'"';
+                        $resultPName = mysqli_query($connect,$sqlPName);
+                        while($row = mysqli_fetch_assoc($resultBrand)){
+                            while(list($key,$value) = each($row)){
+                                echo '<option value="'.$value.'">'.$value.'</option>';
+                            }
+                        }           
+                        mysqli_close($connect);
+                    echo'</select>';
+                    
+                    echo '<button id="btnsub">Submit</button><br>';}
+            ?>
         </form>
     </div>    
     
--- cashier/index2.php
+++ cashier/index2.php
@@ -7,6 +7,34 @@
 <body>
     
     <div>
+        Your products :
+        <?php
+            $sqlOrder0 = $_POST['sqlOrder[0]'];
+            $sqlOrder1 = $_POST['sqlOrder[1]'];
+            $sqlOrder2 = $_POST['sqlOrder[2]'];
+            $sqlOrder3 = $_POST['sqlOrder[3]'];
+            $sqlOrder4 = $_POST['sqlOrder[4]'];
+            $arrPro = array($sqlOrder0,$sqlOrder1,$sqlOrder2,$sqlOrder3,$sqlOrder4);
+            $connect = mysqli_connect("localhost","root","","computerstore");
+            // $sqlBrand = 'SELECT PName,PDetail,Price,Unit FROM product inner join brand on product.BrandID = brand.BrandID
+            // inner join stock on product.ProductID = stock.ProductID';
+            $resultBrand = mysqli_query($connect,$sqlPro);
+            echo '<table border=1>';
+            echo '<th>Product name</th><th>Product detail</th>
+            <th>price</th><th>Unit</th>';
+            while($row = mysqli_fetch_assoc($resultBrand)){
+                echo '<tr>';
+                while(list($key,$value) = each($row)){
+                    echo '<td>'.'<option value="'.$value.'">'.$value.'</option>'.'</td>';
+                }
+            }
+            echo '</table>';           
+            mysqli_close($connect);
+        ?>
+        <a href="index.php">Back</a>
+    </div>
+
+    <!-- <div>
         <?php
             echo $_POST['select_Brand'];
             echo $_POST['product_name'];
@@ -28,7 +56,7 @@
             echo '</table>';
             mysqli_close($connect);
         ?>
-    </div>
+    </div> -->
 </body>
 
 </html>
