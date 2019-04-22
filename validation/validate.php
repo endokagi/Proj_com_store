0963a8c1a33465384d46c1e13c1ddf966f3b04ea
Nantipat Suwanwong <scemisnis@gmail.com>
2019-04-22 21:01:26 +0700

    search type text

--- validation/validate.php
+++ validation/validate.php
@@ -1,7 +1,7 @@
 <?php
 // define variables and set to empty values
-$name = $email = $gender = $comment = $website = "";
-
+//$name = $email = $gender = $comment = $website = "";
+/*
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = test_input($_POST["name"]);
   $email = test_input($_POST["email"]);
@@ -9,11 +9,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $comment = test_input($_POST["comment"]);
   $gender = test_input($_POST["gender"]);
 }
-
+*/
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
   return $data;
 }
-?>
