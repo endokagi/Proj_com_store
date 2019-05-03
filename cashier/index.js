8405dfcef06005f4f2dbd7a25fb8aa8481e7430a
EnDoZaKi <38246228+endokagi@users.noreply.github.com>
2019-05-03 21:44:16 +0700

    Revert "3/5/62"
    This reverts commit e151bc73cdfe07d534473ef5ee1f0a5ee443643f.

--- cashier/index.js
+++ cashier/index.js
@@ -1,10 +1,26 @@
-$(function(){
-    $("#Add_search").click(function(){
+$(function () {
+
+    // $("#cancel_sel_cus").click(function () {
+    //     $(location).attr('href', '../index.php')
+    // });
+
+    // select_customer from cashier/index
+    $("#form_sel_cus").submit(function () {
+        var Cname = $("#Cname").val();
+        console.log(Cname);
+        if (Cname == "null") {
+            alert("Please Choose Name");
+            return false;
+        } else return true;
+    });
+
+
+
+    // 
+    $("#search_add_btn").click(function () {
         var select_brand = $("#select_brand").val();
-        console.log(select_brand)
-        if(select_brand=="null"){
+        if (select_brand == "null") {
             alert("Please Choose Brand");
         }
-        $("#show_select").show();
     });
 });
